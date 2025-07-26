c7fb2040ef20cdcc4ef1e5e2f1974dc00f157656
KaziHossain15 <kazhoss1@gmail.com>
2025-07-26 14:49:22 -0400

    created the html file

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, #request
+from flask import Flask, redirect, request
 
 app = Flask(__name__) # creates a Flask app instance (object)
 
 tasks = [] # initializes an empty list to store tasks
 
-#@app.route('/', methods=['GET', 'POST']) # defines route for home page (GET= to retrieve the data, POST= to handle submission)
+@app.route('/', methods=['GET', 'POST']) # defines route for home page (GET= to retrieve the data, POST= to handle submission)
+def index():
+    if request.method == 'POST': # checks if the form is submitted
+        task = request.form.get('task') # retrieves the task from the form data
+        if task: # checks if the task is not empty
+            tasks.append(task) # adds the task to the list
+        return redirect('/') # redirects to the home page after submission
